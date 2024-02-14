dff66e89daf3d6de009156f3280fbfa0b064bcac
William Arnold <will748@gmail.com>
2024-02-14 19:06:49 +0900

    Small fixes for python 3

--- examples/cinc17/setup.sh
+++ examples/cinc17/setup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-url=https://www.physionet.org/challenge/2017/
+url=https://www.physionet.org/files/challenge-2017/1.0.0
 
 mkdir data && cd data
 
-curl -O $url/training2017.zip
+curl -o training2017.zip $url/training2017.zip?download
 unzip training2017.zip
-curl -O $url/sample2017.zip
+curl  -o sample2017.zip $url/sample2017.zip?download
 unzip sample2017.zip
 curl -O $url/REFERENCE-v3.csv
 
 cd ..
 
 python build_datasets.py
+
